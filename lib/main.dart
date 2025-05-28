372695657c9fe016a66bb7e0b0f8fc820bc4f6cb
Giovannihr2004 <giovannihr2004@email.com>
2025-05-28 11:51:58 -0500

    main.dart actualizado con ruta inicial y navegación completa - 280520251149

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
-﻿import 'package:flutter/material.dart';
+﻿// 📄 main.dart
+// 🕓 Última actualización: 2025-05-28 11:48 (GMT-5)
+
+import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'firebase_options.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/reset_password_screen.dart';
 import 'screens/home_screen.dart';
 import 'screens/profile_screen.dart';
 
@@ -25,8 +32,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
         useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: '/welcome',
       routes: {
+        '/welcome': (context) => const WelcomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/reset': (context) => const ResetPasswordScreen(),
         '/': (context) => const HomeScreen(),
         '/profile': (context) => const ProfileScreen(),
       },
